9098506259a8e823a4a8dea2ca19c07c9d3d058d
Cristopher <cristophernamchee12@gmail.com>
2021-08-12 12:02:11 +0700

    :beers: chore: Temporarily remove test from publish flow

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,16 @@ import type { Config } from '@jest/types';
 
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
   verbose: true,
   transform: {},
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
 };
 
 export default config;
